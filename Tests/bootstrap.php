d9a436eed5849f5065385d57f0821ff7f9682807
Philipp Rieber <github@webflips.net>
2012-02-19 22:53:41 +0100

    update .gitignore remove old bootstrap mechanism for phpunit update phpunit config

--- Tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors', true);
-error_reporting(-1);
-require_once dirname(__DIR__).'/vendor/.composer/autoload.php';
-//if (file_exists($file = __DIR__.'/autoload.php')) {
-//    require_once $file;
-//} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-//    require_once $file;
-//}
